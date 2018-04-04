faf5103aaea811d3a3bafb59ac729f6d621ff166
willow <ylmap@126.com>
2018-04-04 18:44:40 +0800

    s1_pC

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,25 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.*;
+import java.util.List;import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+		 List<String> collection3 = new ArrayList<>();
+        for (String s1 : collection1) {
+            for (List<String> s2 : collection2.values())
+			{
+                if (s1.equals(s2)) {
+                    collection3.add(s1);
+                    break;
+                }
+			}
+
+        }
+        return collection3;
     }
 }
