d42dd9f4f391b0784b2d31b5899cbddd69755419
willow <ylmap@126.com>
2018-04-04 18:47:50 +0800

    s2_pC

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,5 +1,7 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +9,50 @@ public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
 
-        return null;
+         Map<String, Integer> Result = new HashMap<String, Integer>();
+        for(String m:collection1){
+            if(m.contains("-"))
+            {
+                String[] Str = m.split("-");
+                String keyStr = Str[0];
+                int valueStr = Integer.valueOf(Str[1]);
+                if (Result.get(keyStr) == null) {
+                    Result.put(keyStr,valueStr);
+                } else {
+                    Result.put(keyStr, Result.get(keyStr) + valueStr);
+                }
+            }
+            else if(m.contains("[")&&m.contains("]"))
+            {
+                String[] Str = m.split("\\[|\\]");
+                String keyStr = Str[0];
+                int valueStr = Integer.valueOf(Str[1]);
+                if (Result.get(keyStr) == null) {
+                    Result.put(keyStr,valueStr);
+                } else {
+                    Result.put(keyStr, Result.get(keyStr) + valueStr);
+                }
+            }
+            else if(m.contains(":"))
+            {
+                String[] Str = m.split(":");
+                String keyStr = Str[0];
+                int valueStr = Integer.valueOf(Str[1]);
+                if (Result.get(keyStr) == null) {
+                    Result.put(keyStr,valueStr);
+                } else {
+                    Result.put(keyStr, Result.get(keyStr) + valueStr);
+                }
+            }
+            else {
+                if (Result.get(m) == null) {
+                    Result.put(m, 1);
+                } else {
+                    Result.put(m, Result.get(m) + 1);
+                }
+            }
+        }
+
+        return Result;
     }
 }
