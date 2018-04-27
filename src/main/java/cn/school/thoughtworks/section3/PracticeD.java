d0921cdfc04386c59a231730e86523c48cf44323
willow <ylmap@126.com>
2018-04-28 00:06:02 +0800

    rewrite and fixbug

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,43 +1,31 @@
 package cn.school.thoughtworks.section3;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        Map<String, Integer> collection3 = new HashMap<String, Integer>();
-        for(String m:collectionA){
-            if(m.contains("-"))
-            {
-                String[] Str = m.split("-");
-                String keyStr = Str[0];
-                int valueStr = Integer.valueOf(Str[1]);
-                collection3.put(keyStr,valueStr+1);
+        
+        
+         Set<String> collec1Set = new HashSet<>(collectionA);
+        Map<String, Integer> result = new HashMap<>();
+        for (String i :collec1Set) {
+            Integer num = Collections.frequency(collectionA, i);
+            if(i.contains("-")){
+                String[] splitStr = i.split("-");
+                i = splitStr[0];
+                num = Integer.valueOf(splitStr[1]);
             }
-            else {
-                if (collection3.get(m) == null) {
-                    collection3.put(m, 1);
-                } else {
-                    collection3.put(m, collection3.get(m) + 1);
-                }
-           }
+            result.put(i, num);
         }
-
-
-        for(int i=0;i<object.get("value").size();i++)
-        {
-            int newKey=collection3.get(object.get("value").get(i));
-            if (collection3.containsKey(object.get("value").get(i)))
-            {
-                if (newKey>=3) {
-                    newKey-=newKey/3;
-                    collection3.put(object.get("value").get(i),newKey);
-                }
+        for(int i=0;i<object.get("value").size();i++) {
+            int newkey=result.get(object.get("value").get(i));
+            if (result.containsKey(object.get("value").get(i))) {
+                newkey-=newkey/3;
+                result.put(object.get("value").get(i),newkey);
             }
         }
 
-        return collection3;
+        return result;
+
     }
 }
